dca40782e830263eb9d02389e9151c7f8bd84c33
env-gaurav <gaurav2528singh@gmail.com>
2025-01-29 23:52:22 +0530

    introduced functions in the atm code

--- atm.py
+++ atm.py
@@ -1,3 +1,17 @@
+total_balance = 0.0
+
+def addBalance(amt):
+    global total_balance
+    total_balance = total_balance + amt
+    return total_balance
+
+def withdrawBalance(amt):
+    global total_balance
+    if total_balance <= 0:
+        return print("Insufficient Balance")
+    total_balance -= amt
+    return total_balance
+
 atm=f'''
 {"-"*30}        
              ATM
@@ -13,9 +27,13 @@ user_choice=int(input("enter your choice: "))
 if user_choice == 1:
     print("PIN GEN")
 elif user_choice == 2:
-    print("CASH DEPOSIT")
+    cd = float(input("ENTER AMOUNT: "))
+    addBalance(cd)
+    print(f"NEW BALANCE: {total_balance}")
 elif user_choice == 3:
-    print("WITHDRAWL")
+    cw = float(input("ENTER AMOUNT: "))
+    withdrawBalance(cw)
+    print(f"NEW BALANCE: {total_balance}")
 elif user_choice == 4:
     print("BAL ENQUIRY")
 else:

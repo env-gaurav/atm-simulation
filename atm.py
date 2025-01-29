4f8b60ea3c568ba40a941ffc0a35f456a1136eeb
env-gaurav <gaurav2528singh@gmail.com>
2025-01-30 01:26:50 +0530

    implemented pin authorisation

--- atm.py
+++ atm.py
@@ -1,4 +1,13 @@
 total_balance = 0.0
+pin = 1111
+attempts = 2
+
+def validatePin(user_pin):
+    global pin
+    if pin == user_pin:
+        return True
+    return False
+
 
 def addBalance(amt):
     global total_balance
@@ -26,24 +35,35 @@ atm=f'''
 
 {"-"*30}
 '''
-print(atm)
 while True:
-    user_choice=int(input("enter your choice: "))
-    if user_choice== 5:
-        print("Thank You for using ATM")
-        break
-    if user_choice == 1:
-        print("PIN GEN")
-    elif user_choice == 2:
-        cd = float(input("ENTER AMOUNT: "))
-        addBalance(cd)
-        print(f"NEW BALANCE: {total_balance}")
-    elif user_choice == 3:
-        cw = float(input("ENTER AMOUNT: "))
-        withdrawBalance(cw)
-        print(f"NEW BALANCE: {total_balance}")
-    elif user_choice == 4:
-        print("Total Balance: ", total_balance )
+    user_pin= int(input("ENTER PIN: "))
+    if not validatePin(user_pin):
+        print(f" INCORRECT PIN {attempts} ATTEMPTS LEFT")
+        if attempts == 0:
+            print("THANK YOU FOR USING ATM")
+            break
+        else:
+            attempts -= 1
+            continue
     else:
-        print("Acceptable Inputs are 1-5 " )
-        break
+        print(atm)
+        while True:
+            user_choice=int(input("enter your choice: "))
+            if user_choice== 5:
+                print("Thank You for using ATM")
+                break
+            if user_choice == 1:
+                print("PIN GEN")
+            elif user_choice == 2:
+                cd = float(input("ENTER AMOUNT: "))
+                addBalance(cd)
+                print(f"NEW BALANCE: {total_balance}")
+            elif user_choice == 3:
+                cw = float(input("ENTER AMOUNT: "))
+                withdrawBalance(cw)
+                print(f"NEW BALANCE: {total_balance}")
+            elif user_choice == 4:
+                print("Total Balance: ", total_balance )
+            else:
+                print("Acceptable Inputs are 1-5 " )
+                break
